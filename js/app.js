55685ddeb9d694db58c0add2f9c7a716ca009ff5
unknown <julie.kim520@gmail.com>
2020-02-28 23:42:50 +1100

    change projects

--- js/app.js
+++ js/app.js
@@ -36,7 +36,7 @@ const sendMessage = (data) => {
   let url = "https://meetup-ape.herokuapp.com/api/contact/send-message"; 
   // open a connection 
   xhr.open("POST", url, true); 
-  xhr.withCredentials = true
+  xhr.withCredentials = false
 
   // Set the request header i.e. which type of content you are sending 
   xhr.setRequestHeader("Content-Type", "application/json"); 
@@ -76,3 +76,49 @@ function windowOnClick(event) {
 
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
+
+ // Navigation scroll
+  // Select all links with hashes
+  // https://css-tricks.com/snippets/jquery/smooth-scrolling/
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $("html, body").animate(
+            {
+              scrollTop: target.offset().top
+            },
+            1000,
+            function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) {
+                // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              }
+            }
+          );
+        }
+      }
+    });
